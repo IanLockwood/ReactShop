0f49ae6f762ec4f8e97fd592bdcb0a97c02fbdd7
Ian Lockwood <mrianlockwood@gmail.com>
2024-05-22 16:23:05 -0400

    Commit before going down a different path with testing

--- react-shop/src/components/MenuPage/MenuPage.test.js
+++ react-shop/src/components/MenuPage/MenuPage.test.js
@@ -1,3 +1,11 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
+import renderer from 'react-test-renderer';
+import { MenuPage } from "./MenuPage";
+import { menuItemsList } from "./menuItemsList";
+
+it('changes the class when hovered', () => {
+  const component = renderer.create(
+    <MenuPage />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
